6b0b286b36451f381e0743bbc00f6a0f8e002eca
ChadMcIntire <chadmcintirecloud@gmail.com>
2020-06-22 15:33:21 -0600

    The id field was not throwing if error
    Why this change is needed:
    The id field had no error handling if it was a float.
    The next value wasn't initialized.
    
    What this change accomplishes:
    This changed allowed the stack to grow for next. This also
    added much needed error handling to the class.
    
    Issue Number:
    
    Related Issues:

--- create_node.py
+++ create_node.py
@@ -1,4 +1,9 @@
 class Node:
     def __init__(self, name, personal_id):
+        if type(personal_id) != int:
+            raise NameError('Incorrect type, please return an int for personal id')
         self.name = name
         self.personal_id = personal_id
+        self.next = None
+        
+        
--- tests/test_nodes/node_test.py
+++ tests/test_nodes/node_test.py
@@ -6,12 +6,28 @@ import unittest
 class TestNode(TestCase):
     def setUp(self):
         self.node = Node('John', 123)
+
+    def tearDown(self):
+        del self.node
+
     def test_check_id_initialization(self):
        self.assertEqual(self.node.personal_id, 123) 
+
     def test_check_id_is_int(self):
         self.assertEqual(type(self.node.personal_id), type(111))
+
     def test_check_id_is_a_float(self):
         self.assertNotEqual(type(self.node.personal_id), type(123.))
+
+    def test_assert_error_if_float(self):
+        with self.assertRaises(Exception) as context:
+            Node('John', 123.)
+        self.assertTrue('Incorrect type, please return an int for personal id' in str(context.exception))
+
     def test_name_initialization(self):
-       self.assertEqual(self.node.name, 'John') 
-        
+        self.assertEqual(self.node.name, 'John') 
+
+    def test_next_starts_as_none(self):
+        self.assertEqual(self.node.next, None) 
+    
+    
